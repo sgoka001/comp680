2ffa5dc1cc57b67f498e6a162e40788a3c9a4e80
Srisri Gokanapudy <srisri.gokanapudy@abbott.com>
2020-03-04 11:48:11 -0800

    Added authentication, login logout buttons

--- Startup.cs
+++ Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace comp680
 {
@@ -23,6 +27,67 @@ namespace comp680
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Add authentication services
+            services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("Auth0", options => {
+            // Set the authority to your Auth0 domain
+            options.Authority = $"https://dev-ib3b277s.auth0.com";
+
+            // Configure the Auth0 Client ID and Client Secret
+            options.ClientId = "e4Kx81auCERh4zfcSGQHEkotAFDSUPbg";
+                    options.ClientSecret = "rjeG4OKh98BCEZNKn1GLTjGCleieBv8qgNco1M33FkqRGpzEki9GQBii5iaK0Ngl";
+
+            // Set response type to code
+            options.ResponseType = OpenIdConnectResponseType.Code;
+
+            // Configure the scope
+            options.Scope.Add("openid");
+
+            // Set the callback path, so Auth0 will call back to http://localhost:3000/callback
+            // Also ensure that you have added the URL as an Allowed Callback URL in your Auth0 dashboard
+            options.CallbackPath = new PathString("/callback");
+
+            // Configure the Claims Issuer to be Auth0
+            options.ClaimsIssuer = "Auth0";
+
+            options.Events = new OpenIdConnectEvents
+            {
+                // handle the logout redirection
+                OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        var logoutUri = $"https://dev-ib3b277s.auth0.com/v2/logout?client_id=e4Kx81auCERh4zfcSGQHEkotAFDSUPbg";
+
+                        var postLogoutUri = context.Properties.RedirectUri;
+                        if (!string.IsNullOrEmpty(postLogoutUri))
+                        {
+                            if (postLogoutUri.StartsWith("/"))
+                            {
+                        // transform to absolute
+                        var request = context.Request;
+                                postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                            }
+                            logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                        }
+
+                        context.Response.Redirect(logoutUri);
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
             services.AddControllersWithViews();
         }
 
@@ -41,9 +106,11 @@ namespace comp680
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
