d658f386e24d22e88fba826b8bcf607a2b9697eb
Jeff Sakaida <jeffsakaida@yahoo.com>
2020-04-22 18:18:06 -0700

    Added NFL and NHL leaders and removed other tabs. Kept rebounding tab to change to soccer. Made other changes to HomeController to show on home page

---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace comp680.Models
+{
+    public class Team
+    {
+        public string TeamName { get; set; }
+        public string TeamDisplayName { get; set; }
+        public string TeamAbbreviation { get; set; }
+        public string TeamShortDisplayName { get; set; }
+    }
+}
